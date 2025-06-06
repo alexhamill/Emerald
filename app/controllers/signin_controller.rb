677102236af55f7212a6fe7be87a5c7ed22be394
alexhamill <alexander.hamill@icloud.com>
2025-06-06 19:56:33 -0400

    idk

---
+++ app/controllers/signin_controller.rb
@@ -0,0 +1,6 @@
+class SigninController < ApplicationController
+  def signin
+  end
+  def index
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,10 @@
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
 # Example:
+
+Product.create(name: "T-shirt", price: 19.99, description: "A cool t-shirt")
+Product.create(name: "Hat", price: 9.99, description: "A stylish hat")
+Product.create(name: "Mug", price: 12.50, description: "A coffee mug")
 #
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)

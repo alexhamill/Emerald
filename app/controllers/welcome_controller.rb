2eedaa9edeca8fdc434c29636745e44a15401134
alexhamill <alexander.hamill@icloud.com>
2025-06-12 12:51:28 -0400

    pritier

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-    @title = "Welcome to Our Application"
+    @title = "Hi Use the Mine to earn emeralds and buy products!"
   end
 end
